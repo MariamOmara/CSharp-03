ff02d3caea9f9b131b4733d14199ed2069fb09e8
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 18:18:37 +0300

    Q3

--- Program.cs
+++ Program.cs
@@ -4,19 +4,8 @@
 //    {
 //        static void Main(string[] args)
 //        {
-//            //Q2
-//            Book B1 = new Book();
-
-//            Console.WriteLine(B1.ToString());
-//            Console.WriteLine(B1.Equals(B1));
-//            Console.WriteLine(B1.GetHashCode());
-//            Console.WriteLine(B1.GetType());
+//            //Q3
+//            int pages = 464;
 //        }
 //    }
 //}
-//class Book
-//{
-//    public string Title { get; set; }
-//    public int Pages { get; set; }
-//}
-
