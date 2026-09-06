5d3e1232413afeda57db7e1183c717e0bb7d1e89
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 18:14:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C_ASS2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            object obj = book;
+
+            Console.WriteLine(obj);
+        }
+    }
+}
+class Book
+{
+    public string Title { get; set; }
+    public int Pages { get; set; }
+}
+
