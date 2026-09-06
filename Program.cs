f9ca5113fca2db8a75713354e8903273a7cb2c90
Mariam Omara <mariamomara2005@gmail.com>
2026-09-06 18:15:27 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-﻿namespace C_ASS2
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Book book = new Book();
+﻿//namespace C_ASS2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Q1
+//            Book book = new Book();
 
-            object obj = book;
+//            object obj = book;
 
-            Console.WriteLine(obj);
-        }
-    }
-}
-class Book
-{
-    public string Title { get; set; }
-    public int Pages { get; set; }
-}
+//            Console.WriteLine(obj);
+//        }
+//    }
+//}
+//class Book
+//{
+//    public string Title { get; set; }
+//    public int Pages { get; set; }
+//}
 
